2978fe7698eb319ee1ad4aa22ec9973f6fbc1a4a
Franvz <franco.gonzalez12345123@gmail.com>
2024-06-12 17:31:50 -0300

    remove comments in getJoke.js

--- src/logic/getJoke.js
+++ src/logic/getJoke.js
@@ -1,5 +1,5 @@
 export async function getJoke (jokeType) {
-  const res = await fetch(`https://official-joke-api.appspot.com/jokes/${jokeType}/random`) // Hacer de diferentes Tipos jokes/general
+  const res = await fetch(`https://official-joke-api.appspot.com/jokes/${jokeType}/random`)
   const data = await res.json()
   const { setup, punchline } = data[0]
   return [setup, punchline]
